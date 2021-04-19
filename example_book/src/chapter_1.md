0162c7b5d2a3e88b385f060cc1c31bf6796caffb
Aleti <alticodes@gmail.com>
2021-04-19 20:33:54 +0300

    Initial uwuifier

---
+++ example_book/src/chapter_1.md
@@ -0,0 +1,91 @@
+# Unofficial Bevy Cheat Book
+
+Concise practical reference to the [Bevy game engine](https://github.com/bevyengine/bevy).
+
+Covers useful syntax, features, programming patterns, and solutions for common game development tasks.
+
+Designed to be easy to read, straight to the point, using simple language to focus on the important information.
+
+While rich in content, this book does not aim to be exhaustive or complete. It
+cannot feasibly cover every feature of bevy. It is my best attempt to teach the
+most practically-relevant aspects.
+
+The book has several sections:
+
+ - [Bevy Cheatsheet](./cheatsheet.md): condensed one-page listing of common Bevy syntax and features
+ - [Bevy Programming](./programming/_index.md): overview of programming in Bevy
+ - [Bevy Features](./features/_index.md): usage notes about some common Bevy features
+ - [Common Pitfalls](./pitfalls/_index.md): common issues or surprises to watch out for
+ - [Advanced Patterns](./patterns/_index.md): non-obvious tricks or techniques; may be controversial
+ - [Bevy Cookbook](./cookbook/_index.md): examples covering common practical use cases
+
+Not intended to be read in order. Jump to whatever is useful to you!
+
+***Welcome! May this book serve you well!***
+
+(don't forget to
+<a class="github-button" href="https://github.com/bevy-cheatbook/bevy-cheatbook" data-icon="octicon-star" aria-label="Star bevy-cheatbook/bevy-cheatbook on GitHub">Star</a>
+the book's [GitHub repository](https://github.com/bevy-cheatbook/bevy-cheatbook) 😉)
+
+## Recommended Additional Resources
+
+Bevy has a rich collection of [official code examples](https://github.com/bevyengine/bevy/tree/latest/examples#examples).
+
+Check out the [awesome-bevy](https://github.com/bevyengine/awesome-bevy) list,
+for community-made resources.
+
+Our community is very friendly and helpful. Feel welcome to join the [Bevy
+Discord](https://discord.gg/bevy) to chat, ask questions, or get involved in the project!
+
+## Stability Warning
+
+Bevy is still a very new and experimental game engine! It has only been public
+for less than a year!
+
+While improvements have been happening at an incredible pace, and development is
+active, Bevy simply hasn't yet had the time to mature.
+
+*There are no stability guarantees and breaking changes happen often!*
+
+Usually, it not hard to adapt to changes with new releases (or even track the
+main git development branch), but you have been warned!
+
+## Maintenance
+
+This version of the book is for Bevy release 0.5.
+
+You may also access [the `next` branch](https://bevy-cheatbook.github.io/next/).
+It contains any WIP efforts to cover newer developments in Bevy's main git
+branch, in preparation for the next release.
+
+I intend to keep this book up-to-date and relevant with every new Bevy release.
+I also try to regularly make general improvements to it, when I can manage it.
+
+## License
+
+Copyright © 2021 Jasen Borisov.
+
+All code in the book is provided under the [MIT-0 License](https://github.com/bevy-cheatbook/mit-0).
+
+The text of the book is provided under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/).
+
+Exception: If used for the purpose of contribution to the Official Bevy Project
+and/or its Official Documentation, the entire content of the book may be used
+under the [MIT-0 License](https://github.com/bevy-cheatbook/mit-0).
+
+## Contributions
+
+Development of the book is hosted on [GitHub](https://github.com/bevy-cheatbook/bevy-cheatbook).
+
+Please file GitHub Issues for any wrong/confusing/misleading information, as
+well as suggestions for new content you'd like to be added to the book.
+
+Contributions are accepted, with some limitations.
+
+See the [Contributing](./contributing.md) section for all the details.
+
+## Support Bevy
+
+If you like the Bevy Game Engine, you should consider donating to the official
+project, using [GitHub Sponsors](https://github.com/sponsors/cart). Help it be
+sustainable!
---
+++ src/main.rs
@@ -0,0 +1,136 @@
+use crate::uwuify_lib::UwUifier;
+use clap::{App, Arg, ArgMatches, SubCommand};
+use mdbook::book::Book;
+use mdbook::errors::Error;
+use mdbook::preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext};
+use std::io;
+use std::process;
+
+const NAME: &str = "uwuify";
+
+pub fn make_app() -> App<'static, 'static> {
+    App::new("uwuify-preprocessor")
+        .about("A mdbook preprocessor which uwuifies your books uwu")
+        .subcommand(
+            SubCommand::with_name("supports")
+                .arg(Arg::with_name("renderer").required(true))
+                .about("Check whether a renderer is supported by this preprocessor"),
+        )
+}
+
+fn main() {
+    let matches = make_app().get_matches();
+
+    // Users will want to construct their own preprocessor here
+    let preprocessor = UwUifier::new();
+
+    if let Some(sub_args) = matches.subcommand_matches("supports") {
+        handle_supports(&preprocessor, sub_args);
+    } else if let Err(e) = handle_preprocessing(&preprocessor) {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
+
+fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<(), Error> {
+    let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
+
+    if ctx.mdbook_version != mdbook::MDBOOK_VERSION {
+        // We should probably use the `semver` crate to check compatibility
+        // here...
+        eprintln!(
+            "Warning: The {} plugin was built against version {} of mdbook, \
+             but we're being called from version {}",
+            pre.name(),
+            mdbook::MDBOOK_VERSION,
+            ctx.mdbook_version
+        );
+    }
+
+    let processed_book = pre.run(&ctx, book)?;
+    serde_json::to_writer(io::stdout(), &processed_book)?;
+
+    Ok(())
+}
+
+fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
+    let renderer = sub_args.value_of("renderer").expect("Required argument");
+    let supported = pre.supports_renderer(&renderer);
+
+    // Signal whether the renderer is supported by exiting with 1 or 0.
+    if supported {
+        process::exit(0);
+    } else {
+        process::exit(1);
+    }
+}
+
+mod uwuify_lib {
+    use mdbook::{book::Chapter, BookItem};
+    use pulldown_cmark::{Event, Parser};
+    use pulldown_cmark_to_cmark::cmark;
+
+    use super::*;
+
+    /// A no-op preprocessor.
+    pub struct UwUifier;
+
+    impl UwUifier {
+        pub fn new() -> UwUifier {
+            UwUifier
+        }
+    }
+
+    impl Preprocessor for UwUifier {
+        fn name(&self) -> &str {
+            NAME
+        }
+
+        fn run(&self, _: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
+            process(&mut book.sections)?;
+            Ok(book)
+        }
+
+        fn supports_renderer(&self, renderer: &str) -> bool {
+            renderer != "not-supported"
+        }
+    }
+
+    fn process<'a, I>(items: I) -> Result<(), Error>
+    where
+        I: IntoIterator<Item = &'a mut BookItem> + 'a,
+    {
+        for item in items {
+            if let BookItem::Chapter(ref mut chapter) = *item {
+                eprintln!("{}: processing chapter '{}'", NAME, chapter.name);
+
+                let md = uwuify(chapter)?;
+                chapter.content = md;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn uwuify(chapter: &mut Chapter) -> Result<String, Error> {
+        let mut buf = String::with_capacity(chapter.content.len());
+        let events = Parser::new(&chapter.content).map(|e| -> Event {
+            match e {
+                Event::Text(s) => {
+                    let b = s.as_bytes();
+                    let mut temp1 = vec![0u8; uwuifier::round_up16(b.len()) * 16];
+                    let mut temp2 = vec![0u8; uwuifier::round_up16(b.len()) * 16];
+                    let res = uwuifier::uwuify_sse(b, &mut temp1, &mut temp2);
+                    Event::Text(pulldown_cmark::CowStr::from(
+                        std::str::from_utf8(res).unwrap().to_string(),
+                    ))
+                }
+                e => e,
+            }
+        });
+
+        cmark(events, &mut buf, None)
+            .map(|_| buf)
+            .map_err(|err| Error::new(err))
+    }
+}
